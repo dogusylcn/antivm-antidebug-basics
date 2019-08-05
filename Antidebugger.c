6f34ba8f77c16cebd1581e14c9542fd358b5adde
Doğuş Yalçın <35862361+bfied@users.noreply.github.com>
2019-08-05 14:33:04 +0300

    Add files via upload

---
+++ Antidebugger.c
@@ -0,0 +1,34 @@
+#include<windows.h>
+#include<stdio.h>
+
+int main() 
+{
+	register int eax asm("%eax");
+	OutputDebugString("Tubitak");// this changes eax value when program works in debugger
+	
+	if(eax>1) 
+		{
+			printf("debugger detected");
+			return 0;
+		
+		}
+
+	if(IsDebuggerPresent()) // windows api 
+		{
+			printf("debugger detected");
+			return 0;
+		}
+
+	int count = GetTickCount(); // functions running times
+	int count2 = GetTickCount();
+	printf("Tick Count 2 = %d\n",count);
+	printf("Tick Count 1 = %d\n",count2);
+
+	if(count == count2) // we can say no debbuger if no difference both function running time.
+		{
+			printf("No debugger detected");
+		}
+	else printf("debuger detected");	
+
+        return 0;
+}
